e80f610f5450c40b9165022da9993dba98d09cfd
levon-dalakyan <wman8800@gmail.com>
2023-07-30 15:06:41 +0300

    add react

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,7 @@ import { BuildEnv } from "./config/build/types/config";
 
 export default (env: BuildEnv) => {
     const paths = {
-        entry: path.resolve(__dirname, "src", "index.ts"),
+        entry: path.resolve(__dirname, "src", "index.tsx"),
         build: path.resolve(__dirname, "dist"),
         html: path.resolve(__dirname, "public", "index.html"),
     };

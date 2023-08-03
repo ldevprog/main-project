f5f9f2ac65eb78e9deff192349a41ea15fb1b90e
levon-dalakyan <wman8800@gmail.com>
2023-08-03 16:18:08 +0300

    ThemeSwitcher: update component ui

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,7 +1,9 @@
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import cls from "./ThemeSwitcher.module.scss";
 import { Switch } from "shared/ui/Switch";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
+import cls from "./ThemeSwitcher.module.scss";
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -11,8 +13,10 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, changeTheme } = useTheme();
 
     return (
-        <button className={classNames(cls.ThemeSwitcher, {}, [className])}>
+        <div className={classNames(cls.ThemeSwitcher, {}, [className])}>
+            <LightIcon className={classNames(cls.icon, {}, [cls.iconLight])} />
             <Switch theme={theme} handleChange={changeTheme} />
-        </button>
+            <DarkIcon className={classNames(cls.icon, {}, [cls.iconDark])} />
+        </div>
     );
 };

bee7935000ae87f313402126184dc1691b0a7c87
levon-dalakyan <wman8800@gmail.com>
2023-08-04 18:39:25 +0300

    add react-refresh-webpack-plugin

--- src/app/config/build/buildPlugins.ts
+++ src/app/config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins(
@@ -20,5 +21,6 @@ export function buildPlugins(
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-    ];
+        isDev && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean);
 }

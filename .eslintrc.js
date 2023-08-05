5999ef81a7d3b85a401845b5782d0a97ea675f70
levon-dalakyan <wman8800@gmail.com>
2023-08-05 09:29:29 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: "esnext",
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        "react/react-in-jsx-scope": 0,
+        "@typescript-eslint/no-unused-vars": 1,
+        "@typescript-eslint/ban-ts-comment": 0,
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};

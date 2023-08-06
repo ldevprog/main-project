15ea5fa2b0301332e366215572d4ea7f1ffc0053
levon-dalakyan <wman8800@gmail.com>
2023-08-06 16:14:36 +0300

    add bundle-analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,9 @@
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
-import { BuildOptions } from "./types/config";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins(
     options: BuildOptions
@@ -22,5 +23,8 @@ export function buildPlugins(
             __IS_DEV__: JSON.stringify(isDev),
         }),
         isDev && new ReactRefreshWebpackPlugin({ overlay: false }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ].filter(Boolean);
 }

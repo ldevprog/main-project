8a526ecd0974f1ea93ab7143df62facfcbd59db1
levon-dalakyan <wman8800@gmail.com>
2023-08-08 17:40:32 +0300

    add story for Loader component

---
+++ src/shared/ui/Loader/ui/Loader.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Loader } from "./Loader";
+import { ThemeDecorator } from "shared/lib/storybook/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
+
+const meta = {
+    title: "shared/Loader",
+    component: Loader,
+    tags: ["autodocs"],
+    args: {},
+    argTypes: {},
+} satisfies Meta<typeof Loader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const DefaultDark: Story = {
+    decorators: [ThemeDecorator(Theme.DARK)],
+};

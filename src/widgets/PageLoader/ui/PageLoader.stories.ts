c9bdb22c8bdee839493766a976ed41dcc3f53e3d
levon-dalakyan <wman8800@gmail.com>
2023-08-10 14:54:59 +0300

    add story for PageLoader

---
+++ src/widgets/PageLoader/ui/PageLoader.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { PageLoader } from "./PageLoader";
+import { ThemeDecorator } from "shared/lib/storybook/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
+
+const meta = {
+    title: "widgets/PageLoader",
+    component: PageLoader,
+    tags: ["autodocs"],
+    args: {},
+    argTypes: {},
+} satisfies Meta<typeof PageLoader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const DefaultDark: Story = {
+    decorators: [ThemeDecorator(Theme.DARK)],
+};

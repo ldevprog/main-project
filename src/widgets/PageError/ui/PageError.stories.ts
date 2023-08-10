810c9c7f63edd7d8be47d259183842123c99ebb0
levon-dalakyan <wman8800@gmail.com>
2023-08-10 17:21:48 +0300

    PageError improve styles

--- src/widgets/PageError/ui/PageError.stories.ts
+++ src/widgets/PageError/ui/PageError.stories.ts
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { PageError } from "./PageError";
+import { ThemeDecorator } from "shared/lib/storybook/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
 
 const meta = {
     title: "widgets/PageError",
@@ -12,3 +14,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const DefaultDark: Story = {
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
